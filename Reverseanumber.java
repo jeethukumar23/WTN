7a5e9e38681efb477cdf6d7a76d0388f3b53f08b
jeethukumar23 <2300031651@kluniversity.in>
2026-06-12 10:34:51 +0530

    New file

---
+++ Reverseanumber.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Reverseanumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        int result = reverse(num);
+        System.out.println("Reverse of " + num + " is: " + result);
+    }
+
+    public static int reverse(int n) {
+        int reversed = 0;
+        while (n != 0) {
+            int digit = n % 10;
+            reversed = reversed * 10 + digit;
+            n /= 10;
+        }
+        return reversed;
+    }
+}
