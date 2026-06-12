500607c30851782e1fbd6302d3199f47f952efc8
jeethukumar23 <2300031651@kluniversity.in>
2026-06-12 10:39:23 +0530

    New file

---
+++ Sumofdigits.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class sumofdigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        int result = sumOfDigits(num);
+        System.out.println("Sum of digits of " + num + " is: " + result);
+    }
+
+    public static int sumOfDigits(int n) {
+        int sum = 0;
+        while (n != 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
+    }
+}
